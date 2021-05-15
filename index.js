29d20380ee124c82bf40be841f12c30b66d9467a
Manthan Kumbhar <kum.manthan@gmail.com>
2021-05-15 09:22:09 +0530

    backend changes

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
-const app = express();
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
 require("dotenv").config();
 
-app.use(express());
-
 let port = process.env.PORT || 3000;
+const app = express();
+
+const { uploadFile, getFileStream } = require("./s3");
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 app.get("/", (req, res) => {
   res.json({ message: "hello!" });
+});
+
+app.get("/songs/:key", (req, res) => {
+  console.log(req.params);
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+
+  readStream.pipe(res);
+});
+
+app.post("/songs", upload.single("song"), async (req, res) => {
+  const file = req.file;
+  console.log(file);
+
+  const result = await uploadFile(file);
+  console.log(result);
+  await unlinkFile(file.path);
+  var imageKey = await result.Key;
+  res.status(200).send({ imagePath: `/images/${imageKey}` });
 });
 
 app.listen(port, () => {
